2956666c6018f063445b647911a78ec0fe8ef943
altis5526 <altis5526@gmail.com>
2023-12-27 22:21:56 +0800

    Initial Commit

---
+++ MedEasy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MedEasy
+//
+//  Created by 陳祺侑 on 2023/12/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MedEasy/MedEasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedEasyApp.swift
+//  MedEasy
+//
+//  Created by 陳祺侑 on 2023/12/27.
+//
+
+import SwiftUI
+
+@main
+struct MedEasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
